fe392f674f2944fee24c6132f93c54662875f870
BassedWarrior <bassedwarrior724@outlook.com>
2024-04-14 23:54:50 -0600

    Overegineered and still fails in some cases

--- WebDev/Tareas/javascript_basics/Ejercicio_7.js
+++ WebDev/Tareas/javascript_basics/Ejercicio_7.js
@@ -32,6 +32,8 @@ function generate_primes(n) {
         
         ++test_number;
     }
+
+    return primes;
 }
 
 
@@ -45,11 +47,15 @@ function factorize_recursive(x, primes, factors) {
 
     for (let prime of primes) {
         if (x % prime == 0) {
-            x %= prime;
+            x /= prime;
             primes = primes.filter((p) => p <= x ** (1 / 2));
-            return factorize_recursive(x, primes, factors.push(prime)); 
+            factors.push(prime);
+            return factorize_recursive(x, primes, factors); 
         }
     }
+
+    factors.push(x);
+    return factors;
 }
 
 
@@ -70,5 +76,117 @@ function get_divisors(x) {
     let factors = factorize(x);
     let divisors = [1, x];
 
+    // An overtly complicated way of getting the divisors. A dictionary should 
+    // have been used for the factors in order to simplify this.
+    // But the kid wanted to do it using only arrays.
+    //
+    // 23:48 update: Next time...don't do this. It's not worth it.
+    // Don't try to sort the divisors straight away. Just sort them afterwards.
+    for (let l = 1; l <= factors.length / 2; l++) {
+        for (let i = 0; i < factors.length; i++) {
+            let divisor;
+            let wrapped = false;
+            if (i + l > factors.length) {
+                let wrapped = true;
+                divisor = factors.slice(i).reduce((p, f) => p * f)
+                    * factors.slice(0, i + l - factors.length).reduce(
+                        (p, f) => p * f, 1
+                    );
+            } else {
+                divisor = factors.slice(i, i + l).reduce(
+                    (p, f) => p * f, 1
+                );
+            }
+            if (!divisors.includes(divisor)) {
+                let complementary_divisor;
+                if (wrapped) {
+                    complementary_divisor = factors.slice(
+                            i + l - factors.length, i
+                        ).reduce((p, f) => p * f, 1);
+                } else {
+                    complementary_divisor = factors.slice(0, i).reduce(
+                            (p, f) => p * f, 1
+                        ) * factors.slice(i + l).reduce((p, f) => p * f, 1);
+                }
+                if (!divisors.includes(complementary_divisor)
+                    && divisor != complementary_divisor
+                ) {
+                    if (divisor < complementary_divisor) {
+                        divisors.splice(
+                            divisors.length / 2, 0, 
+                            divisor, complementary_divisor
+                        );
+                    } else {
+                        divisors.splice(
+                            divisors.length / 2, 0, 
+                            complementary_divisor, divisor
+                        );
+                    }
+                } else {
+                    divisors.splice(divisors.length / 2, 0, divisor);
+                }
+            }
+        }
+    }
 
+    return divisors;
 }
+
+
+function test_factorize(n, factors) {
+    let result_factors = factorize(n);
+
+    let correct = true;
+    for (let i = 0; i < factors.length; i++) {
+        if (factors[i] != result_factors[i]) {
+            correct = false;
+            break;
+        }
+    }
+
+    if (correct) {
+        console.log("Prueba con", n, "exitosa!");
+    } else {
+        console.log("Prueba con", n, "fallida!");
+    }
+
+    console.log("Expected: ", factors);
+    console.log("Got: ", result_factors);
+}
+
+
+// This also tests for order, but honestly...I'm done. So just...assume they 
+// are ordered. Plz and thank you. The instructions don't require the divisors 
+// to be ordered.
+function test_get_divisors(n, divisors) {
+    let result_divisors = get_divisors(n);
+
+    let correct = true;
+    for (let i = 0; i < divisors.length; i++) {
+        if (divisors[i] != result_divisors[i]) {
+            correct = false;
+            break;
+        }
+    }
+
+    if (correct) {
+        console.log("Prueba con", n, "exitosa!");
+    } else {
+        console.log("Prueba con", n, "fallida!");
+    }
+
+    console.log("Expected: ", divisors);
+    console.log("Got: ", result_divisors);
+}
+
+
+test_factorize(12, [2, 2, 3]);
+test_factorize(73, [73]);
+test_factorize(56, [2, 2, 2, 7]);
+test_factorize(225, [3, 3, 5, 5]);
+
+
+test_get_divisors(12, [1, 2, 3, 4, 6, 12]);
+test_get_divisors(73, [1, 73]);
+test_get_divisors(56, [1, 2, 4, 7, 8, 14, 28, 56]);
+test_get_divisors(225, [1, 3, 5, 9, 15, 25, 45, 75, 225]);
